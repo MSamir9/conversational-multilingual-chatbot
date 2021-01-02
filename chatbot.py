f9221e3092bef04a7aeaf46aefb64a1fe60af095
MSamir9 <75063256+MSamir9@users.noreply.github.com>
2021-01-02 09:11:41 -0500

    Update and rename app.py to chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -28,19 +28,3 @@ training_data = arabic_data_sample+english_data_sample+french_data_sample
 trainer = ListTrainer(chatbot)
 trainer.train(training_data)
 
-from flask import Flask, render_template, request
-
-app = Flask(__name__, template_folder= 'template')
-app.static_folder = 'static'
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/get")
-def get_bot_response():
-    userText = request.args.get('msg')
-    return str(chatbot.get_response(userText))
-
-if __name__ == "__main__":
-    app.run()
