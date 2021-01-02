91bd57e7869db5c39a2dc44958ca73f38309bd1a
MSamir9 <75063256+MSamir9@users.noreply.github.com>
2021-01-02 11:26:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,34 @@
 from flask import Flask, render_template, request
-from chatbot import chatbot
+#from chatbot import chatbot
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+# Creating ChatBot Instance
+chatbot = ChatBot(
+    'Bobot',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch',
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'I am sorry, but I do not understand. I am still learning.',
+            'maximum_similarity_threshold': 0.90
+        }
+    ],
+    database_uri='sqlite:///database.sqlite3'
+)
+
+ # Training with Personal Ques & Ans 
+arabic_data_sample = open('arabic.txt').read().splitlines()
+french_data_sample = open('french.txt').read().splitlines()
+english_data_sample = open('english.txt').read().splitlines()
+
+training_data = arabic_data_sample+english_data_sample+french_data_sample
+
+trainer = ListTrainer(chatbot)
+trainer.train(training_data)
 
 app = Flask(__name__, template_folder= 'template')
 app.static_folder = 'static'
